e4e62d89f4d155e84d36b9402bc376fc3fd41240
Phil Worthington <pworthington27@gmail.com>
2014-01-07 22:51:57 -0800

    Add resque overview in routes

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,7 @@ Snippets::Application.routes.draw do
 
   resources :snippets
   root :to => "snippets#new"
+  mount Resque::Server, :at => '/resque'
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
